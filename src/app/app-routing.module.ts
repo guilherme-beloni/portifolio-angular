5561949cba5dde1328207cf772e408ef373a9784
Guilherme Zacarias Beloni <97005507+guilherme-beloni@users.noreply.github.com>
2023-05-20 11:00:48 -0300

    README

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContentComponent } from './pages/content/content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent,
+  },
+  {
+    path:'content/:id', //serve para adicionar páginas no nosso sistema| Posso apagar o appcomponente do html principal
+    component: ContentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
+import { ContentComponent } from './pages/content/content.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuBarComponent,
     BigCardComponent,
-    SmallCardComponent
+    SmallCardComponent,
+    ContentComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
